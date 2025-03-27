b0192e04a55e0165deffe0736b4ea482f483ea95
jarpulapavan <mepavaniitkgp@gmail.com>
2025-03-27 11:22:52 +0530

    adding math

---
+++ PatternDatabasas/math.h
@@ -0,0 +1,16 @@
+//
+// Created by Pavan on 27-03-2025.
+//
+
+#ifndef MATH_H
+#define MATH_H
+
+
+#include <cstdint>
+
+uint32_t factorial(uint32_t n);
+uint32_t pick(uint32_t n, uint32_t k);
+uint32_t choose(uint32_t n, uint32_t k);
+
+
+#endif //MATH_H
